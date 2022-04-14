8469892367df8161066485d9e87f861ee821ba15
samiaazzouzi <68155401+samiaazzouzi@users.noreply.github.com>
2022-04-14 13:51:20 +0200

    start react app

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          <a href="" className="text-decoration-none">ROSA</a>  
+        </h1>
+      </header>
+      <div className="container">
+
+        <div className="row">
+          <div className="col">
+            left side
+          </div>
+          <div className="col">
+            right side
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
