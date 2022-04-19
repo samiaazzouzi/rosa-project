ffd9284650fd11c5ca24188b70f220f572e7f46f
samiaazzouzi <68155401+samiaazzouzi@users.noreply.github.com>
2022-04-20 00:05:24 +0200

    fix days in the calendar && add icons to buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import Popper from "popper.js";
 import './App.css';
 import LeftComponent from './components/LeftComponent';
 import 'mapbox-gl/dist/mapbox-gl.css';
@@ -15,10 +17,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          <a href="" className="text-decoration-none" style={{ color: "#E4A598"}}>ROSA</a>  
-        </h1>
+        <div className="navbar">
+          <a href="" className="text-decoration-none" style={{color: "#E4A598", fontSize: "2.5em"}}>ROSA</a>  
+            <select id="select1">
+              <option value="free">EN</option>
+              <option value="basic">NL</option>
+              <option value="premium">FR</option>
+            </select>
+        </div>
       </header>
+
       <div className="container">
 
         <div className="row">
--- src/components/RightComponent.tsx
+++ src/components/RightComponent.tsx
@@ -1,12 +1,81 @@
 import React from "react";
-
+import { useRef, useEffect, useState } from 'react';
+import { FaArrowRight } from 'react-icons/fa';
 
 export default function RightComponent() {
-    return (
-        
-        <div style={{ backgroundColor: "#fff" }} className="fixed">
-            <h2>Find availability</h2>
 
+    function getDaysInMonth(year: any, month: any) {
+        return new Date(year, month, 0).getDate();
+    }
+
+    const weekday = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+    let d = new Date();
+    const currentYear = d.getFullYear();
+    const currentMonth = d.getMonth() + 1;
+    const daysInCurrentMonth = getDaysInMonth(currentYear, currentMonth);
+
+    let current_dd = []
+    let daysOfCurrentWeek = []
+
+    let today = d.getDay()
+    let daylimit = 7 - today
+
+    for (let i = 0; i < daylimit; i++) {
+        daysOfCurrentWeek.push(weekday[d.getDay() + i]);
+    }
+
+    for (let i = today; i>0 ; i--)
+    {
+        daysOfCurrentWeek.push(weekday[d.getDay() - i]);
+    }
+
+    for (let i = 0; i < 7; i++) {
+        if ((d.getDate() + i) < daysInCurrentMonth + 1) {
+            current_dd.push(d.getDate() + i)
+        }
+    }
+
+    const [days, setdays] = useState(daysOfCurrentWeek);
+    const [dd, setdd] = useState(current_dd);
+
+    function nextweek() {
+        let daysOfNextWeek = []
+        let next_dd = []
+        let today = d.getDay()
+
+        let daylimit = 7 - today
+
+        for (let i = 0; i < daylimit; i++) {
+            daysOfNextWeek.push(weekday[d.getDay() + i]);
+
+        }
+        for (let i = today; i > 0; i--) {
+            daysOfNextWeek.push(weekday[d.getDay() - i]);
+        }
+        let j = 1;
+        for (let i = 7; i < 14; i++) {
+            if (d.getDate() + i < daysInCurrentMonth + 1) { next_dd.push(d.getDate() + i) }
+            else {
+                next_dd.push(j)
+                j++
+            }
+        }
+        setdays(daysOfNextWeek);
+        setdd(next_dd)
+    }
+    
+    let daysOfPreviousWeek = daysOfCurrentWeek
+    let previous_dd = current_dd
+    function previousweek() {
+        setdays(daysOfPreviousWeek);
+        setdd(previous_dd)
+    }
+   
+    return (
+
+        <div style={{ backgroundColor: "#fff", marginTop: "04%" }} className="fixed">
+
+            <h2>Find availability</h2>
             <form>
                 <label className="container-label ">Is this your first appointment with this practitioner?</label>
                 <div className="container" style={{ marginBottom: "10px" }} >
@@ -35,21 +104,23 @@ export default function RightComponent() {
                     </select>
                 </div>
 
+
+
                 <table className="table">
                     <thead>
                         <tr>
                             <th >
-                                <button className="direction-btn" aria-label="true" aria-hidden="false">&#10094;</button>
+                                <button className="direction-btn" aria-label="true" aria-hicurrent_dden="false" onClick={previousweek}>&#10094;</button>
                             </th>
-                            <th className="center">Mo <br></br><span>1</span></th>
-                            <th className="center">Tu <br></br><span>2</span></th>
-                            <th className="center">We <br></br><span>3</span></th>
-                            <th className="center">Th <br></br><span>4</span></th>
-                            <th className="center">Fr <br></br><span>5</span></th>
-                            <th className="center">Sa <br></br><span>6</span></th>
-                            <th className="center">Su <br></br><span>7</span></th>
+                            <th  className="center">{days[0]}<br></br><span>{dd[0]}</span></th>
+                            <th  className="center">{days[1]} <br></br><span>{dd[1]}</span></th>
+                            <th  className="center">{days[2]} <br></br><span>{dd[2]}</span></th>
+                            <th  className="center">{days[3]}<br></br><span>{dd[3]}</span></th>
+                            <th  className="center">{days[4]}<br></br><span>{dd[4]}</span></th>
+                            <th  className="center">{days[5]} <br></br><span>{dd[5]}</span></th>
+                            <th  className="center">{days[6]}<br></br><span>{dd[6]}</span></th>
                             <th >
-                                <button className="direction-btn" aria-label="true" aria-hidden="false">&#10095;</button>
+                                <button className="direction-btn" aria-label="true" aria-hicurrent_dden="false" onClick={nextweek}>&#10095;</button>
                             </th>
                         </tr>
                     </thead>
@@ -210,11 +281,12 @@ export default function RightComponent() {
                             </td>
                         </tr>
                     </tbody>
-                </table>               
+                </table>
             </form>
             <div className="col text-center">
-                <button type="button" className="btn btn-primary btn-lg btn-block appointment-btn">Book Appointment</button>
+                <button type="button" className="btn btn-primary btn-lg btn-block appointment-btn">Book Appointment <FaArrowRight /></button>
             </div>
+
         </div>
     );
 }
